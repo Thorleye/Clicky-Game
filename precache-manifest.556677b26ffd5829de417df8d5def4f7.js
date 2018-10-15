dd4ebad3177709ac492bd7b21a9f21ed0929deaa
Thorley <thorley.eric@gmail.com>
2018-10-15 15:57:11 -0400

    Updates

---
+++ precache-manifest.556677b26ffd5829de417df8d5def4f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9a30e6c6c75344e5bd217bbe9720ef3e",
+    "url": "/Clicky-Game/static/media/footer_lodyas.9a30e6c6.png"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "67d7560a3c16e16d4867",
+    "url": "/Clicky-Game/static/js/main.67d7560a.chunk.js"
+  },
+  {
+    "revision": "762cd21ca94eb77eb0eb",
+    "url": "/Clicky-Game/static/js/1.762cd21c.chunk.js"
+  },
+  {
+    "revision": "67d7560a3c16e16d4867",
+    "url": "/Clicky-Game/static/css/main.db2b4188.chunk.css"
+  },
+  {
+    "revision": "fa264f13d3d17c2fd3dbbd7342440f06",
+    "url": "/Clicky-Game/index.html"
+  }
+];
